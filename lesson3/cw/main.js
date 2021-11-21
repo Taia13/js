3876744f3bea251fd02a595159adf5430e200399
Taia13 <mas.system13@gmail.com>
2021-11-22 01:19:29 +0200

    cw3

--- lesson3/cw/main.js
+++ lesson3/cw/main.js
@@ -16,21 +16,104 @@ let number = [2,17,13,6,22,31,45,66,100,-18];
 //
 //     }
     //перебрати циклом while та вивести  числа тільки з непарним індексом
-let i = 0;
-    while (i< number.length){
-        if(i % 2 !==0){
-            console.log(i);
-            document.write(`<div>${ number [i]}</div>`);
-        }
-           i++;
-    }
+// let i = 0;
+//     while (i< number.length){
+//         if(i % 2 !==0){
+//             console.log(i);
+//             document.write(`<div>${ number [i]}</div>`);
+//         }
+//            i++;
+//     }
     //перебрати циклом for та вивести  числа тільки з непарним індексом
-for (let j = 0; j < number.length; j++) {
-   if(j % 2 !== 0){
-       console.log(j);
-       document.write(`<div>${ number [j]}</div>`);
-
-   }
+// for (let j = 0; j < number.length; j++) {
+//    if(j % 2 !== 0){
+//        console.log(j);
+//        document.write(`<div>${ number [j]}</div>`);
+//
+//    }
+//
+// }
+// перебрати циклом while та вивести  числа тільки парні  значення
+// let i = 0;
+// while (i < number.length){
+//     if ((number[i]%2)===0) {
+//         console.log(number[i]);
+//         document.write(`<div>${number[i]}</div>`);
+//     }
+//
+//     i++;
+// }
+//перебрати циклом for та вивести  числа тільки парні  значення
+// for (let j = 0; j < number.length; j++) {
+//    if ((number[j] % 2 )===0){
+//        console.log(number[j]);
+//        document.write(`<div>${number[j]}</div>`);
+//    }
+//
+// }
+//замінити кожне число кратне 3 на слово "okten"
+// for (let j = 0; j < number.length; j++) {
+//     if ((number[j] % 3) === 0){
+//         number[j] = 'okten';
+//         console.log([j]);
+//         document.write(`<div>${number[j]}</div>`);
+//     }
+//
+// }
+// вивести масив в зворотньому порядку.
+// for (let j = number.length - 1; j >= 0; j--) {
+//     console.log(number[j]);
+//     document.write(`<div>${number[j]}</div>`);
+//
+// }
+//перебрати його циклом while
+// let j = number.length - 1;
+// while (j >= 0){
+//     console.log(number[j]);
+//     document.write(`<div>${number[j]}</div>`);
+//
+//     j--;
+// }
+//перебрати циклом while та вивести  числа тільки з непарним індексом
+let j = number.length - 1;
+while (j>=0){
+    if (j % 2 !==0){
+        console.log(j);
+        document.write(`<div>${number[j]}</div>`);
+    }
+    j--;
+}
+// перебрати циклом for та вивести  числа тільки з непарним індексом
+for (let i = number.length - 1; i >= 0; i--) {
+    if (i%2 !== 0 ){
+        console.log(i);
+        document.write(`<div>${number[i]}</div>`);
+    }
 
 }
 // перебрати циклом while та вивести  числа тільки парні  значення
+j = number.length -1;
+while (j>=0){
+    if ((number[j] % 2)===0){
+        console.log(number[j]);
+        document.write(`<div>${number[j]}</div>`);
+    }
+
+    j--;
+}
+//перебрати циклом for та вивести  числа тільки парні  значення
+for (let i = number.length - 1; i >= 0; i--) {
+  if ((number[i] % 2) === 0){
+      console.log(number[i]);
+      document.write(`<div>${number[i]}</div>`);
+  }
+
+}
+//замінити кожне число кратне 3 на слово "okten"
+for (let i = number.length - 1; i >= 0; i--) {
+    if ((number[i] % 3)===0){
+        number[i]= 'okten';
+        console.log([i]);
+        document.write(`<div>${number[i]}</div>`);
+    }
+}
